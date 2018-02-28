ddbf9b3c751f332cd9c3bfcf250aadc3130cff21
Katy M <36520823+KatyMcQ@users.noreply.github.com>
2018-02-28 16:50:37 -0600

    Add files via upload
    Mockup files uploaded 2/28/18

---
+++ mockupViewApp/mockupViewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mockupViewApp
+//
+//  Created by Katy McQuaid on 2/28/18.
+//  Copyright © 2018 Katy McQuaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
